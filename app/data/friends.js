312ff377392a3ea97aefec39b4de678e407da250
Aries Lao <aries.lao@gmail.com>
2018-04-21 20:38:39 -0700

    updating for homework submission

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+var friendsArray = [
+  {
+    name:"Peeping Tom",
+    photo:"https://awesomestuff365.com/scary-peeping-tom-window-prop/",
+    scores: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name:"Dirty Sanchez",
+    photo:"http://www.tshirtbordello.com/dirty-sanchez-cantina-t-shirt",
+    score: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name:"Nacho Libre",
+    photo:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa8%2F31%2Fc7%2Fa831c7860d28ffb927f2f3dc233bc96b.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F263179171950139839%2F&docid=zERUhy8IgiI06M&tbnid=3o0s58L3OvQLaM%3A&vet=10ahUKEwjIgYiZ1craAhWpw1QKHQn5DhIQMwjwASgGMAY..i&w=1000&h=1500&bih=624&biw=1280&q=nacho%20libre&ved=0ahUKEwjIgYiZ1craAhWpw1QKHQn5DhIQMwjwASgGMAY&iact=mrc&uact=8",
+    score: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+
+]
+
+
+
+
+  
+
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
